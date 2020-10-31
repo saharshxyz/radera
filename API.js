7381157b0b24a6f0bf6cdac1babfb959056a28ea
Saharsh Yeruva <hello@saharsh.tech>
2020-10-31 19:55:21 -0400

    Change env vars

--- API.js
+++ API.js
@@ -1,31 +1,38 @@
-const {WebAPICallResult, WebClient} = require('@slack/web-api');
+const { WebAPICallResult, WebClient } = require('@slack/web-api');
 
 class API {
-    web;
+	web;
 
-    constructor(SLACK_TOKEN, username) {
-        this.SLACK_TOKEN = SLACK_TOKEN;
-        this.web = new WebClient(this.SLACK_TOKEN);
-        this.username = username;
-        this.commonOptions = {query: `from:@${username}`,};
-    }
+	constructor(OAUTH_ACCESS_TOKEN, username) {
+		this.OAUTH_ACCESS_TOKEN = OAUTH_ACCESS_TOKEN;
+		this.web = new WebClient(this.OAUTH_ACCESS_TOKEN);
+		this.username = username;
+		this.commonOptions = { query: `from:@${username}` };
+	}
 
-    async getPaginationInfo(){
-        const res = await this.web.search.all({...this.commonOptions, count: 100, page: 1});
-        return {
-            files: {
-                total: res.files.pagination.page_count
-            },
-            messages: {
-                total: res.messages.pagination.page_count
-            }
-        }
-    }
+	async getPaginationInfo() {
+		const res = await this.web.search.all({
+			...this.commonOptions,
+			count: 100,
+			page: 1,
+		});
+		return {
+			files: {
+				total: res.files.pagination.page_count,
+			},
+			messages: {
+				total: res.messages.pagination.page_count,
+			},
+		};
+	}
 
-    async get(page, type){
-        const res = await this.web.search[type]({...this.commonOptions, count: 100, page});
-        return res[type].matches;
-    }
-
+	async get(page, type) {
+		const res = await this.web.search[type]({
+			...this.commonOptions,
+			count: 100,
+			page,
+		});
+		return res[type].matches;
+	}
 }
 module.exports = API;
